db1be241dbb7ea8d23df5c5765328c944ed2aeb0
homesin <58244938+homesin@users.noreply.github.com>
2019-12-14 06:00:07 +0800

    v2

--- liff-starter.js
+++ liff-starter.js
@@ -3,9 +3,14 @@ liff.init({
 })
 .then(() => {
     // start to use LIFF's api
-    initializeApp();
+    //initializeApp();
 })
 .catch((err) => {
     //document.getElementById("liffAppContent").classList.add('hidden');
     //document.getElementById("liffInitErrorMessage").classList.remove('hidden');
 });
+
+if (!liff.isLoggedIn()) {
+    // set `redirectUri` to redirect the user to a URL other than the front page of your LIFF app.
+    liff.login();
+}
