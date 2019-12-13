726f85c9799aa5fdfc3d5903e2f9eec7861eecbb
homesin <58244938+homesin@users.noreply.github.com>
2019-12-14 05:41:12 +0800

    v2

---
+++ liff-starter.js
@@ -0,0 +1,11 @@
+liff.init({
+    liffId: 1653571695-gOjprRLE
+})
+.then(() => {
+    // start to use LIFF's api
+    //initializeApp();
+})
+.catch((err) => {
+    //document.getElementById("liffAppContent").classList.add('hidden');
+    //document.getElementById("liffInitErrorMessage").classList.remove('hidden');
+});
